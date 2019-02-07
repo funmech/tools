cd05ce71d0815d7042be9967fd05e42b02bcc1f8
Jianfeng Li <jianfeng.li@pwc.com>
2019-02-07 16:28:39 +1030

    Add lookup route

--- Flask.Dockerfile
+++ Flask.Dockerfile
@@ -1,8 +1,9 @@
 FROM python:3.6-alpine
 
-RUN pip install -U Flask
+RUN pip install -U Flask requests
 
 WORKDIR /app
 COPY flask-app.py /app/app.py
 
+ENV Docker True
 CMD ["python", "app.py"]
--- flask-app.py
+++ flask-app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from os import getenv
+
+from flask import Flask, request
+import requests
+
 
 app = Flask(__name__)
 
@@ -6,5 +10,23 @@ app = Flask(__name__)
 def main():
     return 'Flask Dockerized'
 
+@app.route('/lookup')
+def lookup():
+    timeout = 3
+    try:
+        ns = request.args.get('ns')
+        return requests.get(ns, timeout=timeout, verify=False).text
+    except requests.exceptions.ConnectTimeout:
+        return F"Connectting from url {ns} timed out after {timeout} seconds"
+    except requests.exceptions.ReadTimeout:
+        return F"Reading from url {ns} timed out after {timeout} seconds"
+    except Exception as err:
+        return str(err)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    if getenv('Docker', False):
+        app.run(debug=True, host='0.0.0.0')
+    else:
+        # this is for local
+        app.run(debug=True)
