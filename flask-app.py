75c35309aa0bedb8696fe860d7cd91a735356322
Jianfeng Li <jianfeng.li@pwc.com>
2019-02-15 16:31:38 +1030

    Add tcp port check

--- flask-app.py
+++ flask-app.py
@@ -5,6 +5,7 @@ import requests
 
 
 app = Flask(__name__)
+TIMEOUT = 3
 
 @app.route('/')
 def main():
@@ -12,16 +13,32 @@ def main():
 
 @app.route('/lookup')
 def lookup():
-    timeout = 3
     try:
         ns = request.args.get('ns')
-        return requests.get(ns, timeout=timeout, verify=False).text
+        return requests.get(ns, timeout=TIMEOUT, verify=False).text
     except requests.exceptions.ConnectTimeout:
-        return F"Connectting from url {ns} timed out after {timeout} seconds"
+        return F"Connectting from url {ns} timed out after {TIMEOUT} seconds"
     except requests.exceptions.ReadTimeout:
-        return F"Reading from url {ns} timed out after {timeout} seconds"
+        return F"Reading from url {ns} timed out after {TIMEOUT} seconds"
     except Exception as err:
         return str(err)
+
+
+@app.route('/checkport')
+def tcpCheck():
+    ip = request.args.get('ip')
+    port = int(request.args.get('port'))
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(TIMEOUT)
+    try:
+        s.connect((ip, port))
+        s.shutdown(socket.SHUT_RDWR)
+        return F"{ip}:{port} is open"
+    except:
+        return F"{ip}:{port} is closed/not accessible"
+    finally:
+        s.close()
 
 
 if __name__ == '__main__':
