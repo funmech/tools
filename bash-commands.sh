de080902872686f0759674ad39fcc1e5217f3e63
Jianfeng Li <jianfeng.li@pwc.com>
2019-04-03 15:22:44 +1030

    Add place for saving bash commands

---
+++ bash-commands.sh
@@ -0,0 +1,42 @@
+# get open ports on local network
+netstat -ap tcp
+
+# -i4 means only show ipv4 address and ports -P and -n fast output
+lsof -Pn -i4
+
+# TCP in LISTEN state
+lsof -PiTCP -sTCP:LISTEN
+
+# check if a port is open
+lsof -Pi :8814 -sTCP:LISTEN
+
+# on macOS, add '' after -i
+# or brew install gnu-sed and use gsed
+sed -i '' 's/app\.services\.orms\.orm_base/\.orm_base/' *.py
+
+sed -i ''  -e 's/\(__tablename__ = \".*\)\"/\1\_v2"/' *.py
+
+# delete 4 lines and the matched line
+(g)sed '/Set table arguments/,+4 d' orm_bank_accounts.py
+
+# replace the next line after the pattern 'Unix' with 'hi':
+sed '/Unix/{n;s/.*/hi/}' file
+sed -r '/db.ForeignKey(/{n;s/\"([\w_])\./\1hi/}' orm_bank_accounts.py
+(g)sed -e '/db\.ForeignKey(/{n;s/\(column\=\"\)\(.*\)\./\1\2_v2\./}' orm_bank_accounts.py
+
+# multiline search with awk for not containing v2
+awk "/db.ForeignKey/,/column/" orm_*.py | grep -v v2
+
+
+# https://blog.mozilla.org/webdev/2015/10/27/eradicating-those-nasty-pyc-files/
+find . -name '*.pyc' -delete
+
+# side note: to prevent pyc, run
+export PYTHONDONTWRITEBYTECODE=1
+
+#git graph in terminal
+git log --graph --decorate --pretty=oneline --abbrev-commit
+
+# Rebase local branch with remote branch
+# useful before merge back to remote branch
+git pull --rebase origin branch
