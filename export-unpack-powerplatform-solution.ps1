52b27d23cfa9b0bbb4916711ce64082995eca47f
Li Jianfeng Li <Li-ReDBox@users.noreply.github.com>
2024-11-29 10:29:50 +1030

    Update export-unpack-powerplatform-solution.ps1
    Exit when there is an input error.

--- export-unpack-powerplatform-solution.ps1
+++ export-unpack-powerplatform-solution.ps1
@@ -16,33 +16,34 @@ $workingDir = $Args[1]
 $count = 1
 $jobs = @()
 
-# Check if the file exists
-if (Test-Path $filePath) {
-    # Read the content of the file line by line
-    Get-Content $filePath | ForEach-Object {
-        # Process each line
-        $solution = $_
-        
-        # Example processing: Display each line with some formatting
-        Write-Host "${count}: export $solution"
-        pac solution export --environment https://flinders-dev.crm6.dynamics.com/ --name $solution --path $workingDir\${solution}.zip --overwrite
-        # Execute the jobs in parallel
-        Start-Job $ScriptBlock -ArgumentList $_
-        Start-ThreadJob { pac solution unpack -z $workingDir\$($_.Name) -f $workingDir\$($_.BaseName) }
-        $jobs += Start-ThreadJob -Name "unpack ${solution}.zip" -ScriptBlock {
-            param($solution, $workingDir)
-            pac solution unpack -z $workingDir\${solution}.zip -f $workingDir\$solution 
-        } -ArgumentList $solution,$workingDir
-
-        Write-Host
-        $count++
-    }
-  Wait-Job -Job $jobs
-  
-  foreach ($job in $jobs) {
-      Receive-Job -Job $job
-  }    
-} else {
-    Write-Host "The $filePath does not exist."
+# Check if the list file and workdir exists
+if (-not ((Test-Path -Path $filePath -PathType Leaf ) -and (Test-Path -Path $workingDir -PathType Container))) 
+{
+    Write-Host "Please check to make sure both $filePath and $workingDir do exist."
+    exit
 }
 
+# Read the content of the file line by line
+Get-Content $filePath | ForEach-Object {
+    # Process each line
+    $solution = $_
+    
+    # Example processing: Display each line with some formatting
+    Write-Host "${count}: export $solution"
+    pac solution export --environment https://flinders-dev.crm6.dynamics.com/ --name $solution --path $workingDir\${solution}.zip --overwrite
+    # Execute the jobs in parallel
+    Start-Job $ScriptBlock -ArgumentList $_
+    Start-ThreadJob { pac solution unpack -z $workingDir\$($_.Name) -f $workingDir\$($_.BaseName) }
+    $jobs += Start-ThreadJob -Name "unpack ${solution}.zip" -ScriptBlock {
+        param($solution, $workingDir)
+        pac solution unpack -z $workingDir\${solution}.zip -f $workingDir\$solution 
+    } -ArgumentList $solution,$workingDir
+
+    Write-Host
+    $count++
+}
+Wait-Job -Job $jobs
+
+foreach ($job in $jobs) {
+  Receive-Job -Job $job
+}    
